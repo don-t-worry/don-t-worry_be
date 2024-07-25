70c14a8878e0a53d9b8bcee0a94e8852fa0590f2
simjaeyoun <spqjfgogo@gmail.com>
2024-07-25 12:56:25 +0900

    Initial commit

---
+++ src/main/java/kwangwoon/chambit/dontworry/DontworryApplication.java
@@ -0,0 +1,13 @@
+package kwangwoon.chambit.dontworry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontworryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontworryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kwangwoon/chambit/dontworry/DontworryApplicationTests.java
@@ -0,0 +1,13 @@
+package kwangwoon.chambit.dontworry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontworryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
