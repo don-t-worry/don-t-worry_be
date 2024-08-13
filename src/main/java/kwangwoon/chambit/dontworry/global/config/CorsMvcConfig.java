0f44112f36f94ae16bf383e404eb7e229298f687
simjaeyoun <spqjfgogo@gmail.com>
2024-08-13 17:41:13 +0900

    [Feat] cors오류 관련 config파일 작성

---
+++ src/main/java/kwangwoon/chambit/dontworry/global/config/CorsMvcConfig.java
@@ -0,0 +1,15 @@
+package kwangwoon.chambit.dontworry.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer { // 모든 요청이 컨트롤러에 도달하기 전에 적용됩니다
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .exposedHeaders("Authorization")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
