b2c9e86ada2aa024afa0b5025ce1ba56aae5abc8
simjaeyoun <spqjfgogo@gmail.com>
2024-08-19 20:48:52 +0900

    [Refactor] user와hedgetype의코드 변경

--- src/main/java/kwangwoon/chambit/dontworry/domain/user/enums/HedgeType.java
+++ src/main/java/kwangwoon/chambit/dontworry/domain/user/enums/HedgeType.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum HedgeType {
-    MINIMAL(0,20), BALANCED(20,40), STABLE(40,60);
+    MINIMAL(0,20,"최소 리스크 관리"), BALANCED(20,40, "균형 리스크 관리"), STABLE(40,60, "안정 리스크 관리");
     private final int upperHedgeRatio;
     private final int lowerHedgeRatio;
+    private final String korean;
 
-    HedgeType(int lowerHedgeRatio, int upperHedgeRatio){
+    HedgeType(int lowerHedgeRatio, int upperHedgeRatio,String korean){
         this.lowerHedgeRatio = lowerHedgeRatio;
         this.upperHedgeRatio = upperHedgeRatio;
+        this.korean = korean;
     }
 }
