c497bb54f78356050b6773f4875525e54b7b6331
simjaeyoun <spqjfgogo@gmail.com>
2024-08-31 20:32:36 +0900

    [Feat] 존재하지 않은 토큰이 왔을 때 처리하는 로직 추가

--- src/main/java/kwangwoon/chambit/dontworry/global/security/jwt/filter/JwtExceptionFilter.java
+++ src/main/java/kwangwoon/chambit/dontworry/global/security/jwt/filter/JwtExceptionFilter.java
@@ -6,9 +6,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kwangwoon.chambit.dontworry.global.common.dto.ErrorResponse;
 import kwangwoon.chambit.dontworry.global.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,13 +27,18 @@ public class JwtExceptionFilter extends OncePerRequestFilter {
         try{
             filterChain.doFilter(request, response);
         } catch (JwtException e){
-            response.setStatus(401);
+            response.setStatus(400);
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
             response.setCharacterEncoding("UTF-8");
-            objectMapper.writeValue(response.getWriter(), ResponseDto.addStatus(401,e.getMessage()));
+            objectMapper.writeValue(response.getWriter(), ErrorResponse.builder().status(400).value(e.getMessage()).build());
             // json으로 변환하여 출력대상에 쓰임
             // 첫번째 인자는 출력대상 목적지, 두번째는 json으로 변환되는 객체
             // 여기서는 http의 목적지, 이때 목적지에 값들을 바꾼게 위의 설정들임
+        } catch (IllegalArgumentException e){
+            response.setStatus(400);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
+            objectMapper.writeValue(response.getWriter(), ErrorResponse.builder().status(400).value(e.getMessage()).build());
         }
     }
 }
