db29b6f2a36c51241eb2f19c78a5357e072f34f8
simjaeyoun <spqjfgogo@gmail.com>
2024-08-31 20:29:52 +0900

    [Refactor] redicrection주소를 domainconfig이용해서 변수로 변경

--- src/main/java/kwangwoon/chambit/dontworry/global/security/oauth/handler/Oauth2FailureHandler.java
+++ src/main/java/kwangwoon/chambit/dontworry/global/security/oauth/handler/Oauth2FailureHandler.java
@@ -3,16 +3,19 @@ package kwangwoon.chambit.dontworry.global.security.oauth.handler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kwangwoon.chambit.dontworry.global.config.DomainConfig;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+import static kwangwoon.chambit.dontworry.global.config.DomainConfig.FrontServer;
+
 @Component
 public class Oauth2FailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        response.sendRedirect("http://localhost:3000/");
+        response.sendRedirect(FrontServer.getPresentAddress());
     }
 }
