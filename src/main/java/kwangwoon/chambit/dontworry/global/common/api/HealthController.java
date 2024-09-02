ddfef94450dbffbce21288bc4529cca09c9805f5
simjaeyoun <spqjfgogo@gmail.com>
2024-09-02 21:06:01 +0900

    [Refactor] 스웨거 설정 및 인자 변경 1. 스웨거 상에서 보이지 않도록 변경 2. api 설명 추가 3. token정보를 받을 때 인자 타입 변경

--- src/main/java/kwangwoon/chambit/dontworry/global/common/api/HealthController.java
+++ src/main/java/kwangwoon/chambit/dontworry/global/common/api/HealthController.java
@@ -1,21 +1,27 @@
 package kwangwoon.chambit.dontworry.global.common.api;
 
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kwangwoon.chambit.dontworry.global.security.oauth.dto.CustomOauth2ClientDto;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Hidden
 public class HealthController {
     @GetMapping("/")
+    @Operation(summary = "토큰 정보 없이 테스트 할 수 있는 api" , description = "cicd용 health check때 사용")
     public String getHealthCheck(){
         return "hi";
     }
 
     @GetMapping("/test/user/data")
-    public String getUserData(@AuthenticationPrincipal Authentication authentication){
-        CustomOauth2ClientDto dto = (CustomOauth2ClientDto) authentication.getPrincipal();
-        return dto.getUsername();
+    @Operation(summary = "토큰 정보 있이 테스트 할 수 있는 api")
+    public String getUserData(@AuthenticationPrincipal UserDetails user){
+        return user.getUsername();
     }
 }
