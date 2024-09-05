39e777dd6f42f2500a2a552eae6ea85dbf981b0b
simjaeyoun <spqjfgogo@gmail.com>
2024-09-05 18:24:02 +0900

    [Refactor] 빌더 생성 및 view id 설정

--- src/main/java/kwangwoon/chambit/dontworry/domain/trade/domain/TradeHistory.java
+++ src/main/java/kwangwoon/chambit/dontworry/domain/trade/domain/TradeHistory.java
@@ -3,10 +3,7 @@ package kwangwoon.chambit.dontworry.domain.trade.domain;
 import jakarta.persistence.*;
 import kwangwoon.chambit.dontworry.domain.derivative.domain.Derivative;
 import kwangwoon.chambit.dontworry.domain.trade.enums.TradeType;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -37,5 +34,18 @@ public class TradeHistory {
     @JoinColumn(name = "derivative_id")
     @OnDelete(action = OnDeleteAction.SET_NULL)
     private Derivative derivative;
+
+    @Builder
+    public TradeHistory(Long commission, LocalDateTime time, Long investmentBalance, Long activeInvestments, Long profit, Long price, Long count, TradeType tradeType, Derivative derivative){
+        this.commission = commission;
+        this.time = time;
+        this.investmentBalance = investmentBalance;
+        this.activeInvestments = activeInvestments;
+        this.profit = profit;
+        this.price = price;
+        this.count = count;
+        this.tradeType = tradeType;
+        this.derivative = derivative;
+    }
 
 }
