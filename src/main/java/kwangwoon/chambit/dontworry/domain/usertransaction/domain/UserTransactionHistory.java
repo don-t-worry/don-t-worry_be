5186c1092df9e02f8e4f0d1e5d37901d42ff8a3a
simjaeyoun <spqjfgogo@gmail.com>
2024-09-05 20:03:25 +0900

    [Feat] 시간 입력받도록 하고, 빌더 패턴 추가

--- src/main/java/kwangwoon/chambit/dontworry/domain/usertransaction/domain/UserTransactionHistory.java
+++ src/main/java/kwangwoon/chambit/dontworry/domain/usertransaction/domain/UserTransactionHistory.java
@@ -1,28 +1,32 @@
 package kwangwoon.chambit.dontworry.domain.usertransaction.domain;
 
 import jakarta.persistence.*;
+import jakarta.transaction.UserTransaction;
 import kwangwoon.chambit.dontworry.domain.user.domain.User;
 import kwangwoon.chambit.dontworry.domain.usertransaction.enums.AccountType;
 import kwangwoon.chambit.dontworry.domain.usertransaction.enums.TransactionType;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@ToString(of = {"id" , "funds"})
+@EntityListeners(AuditingEntityListener.class)
+@ToString(of = {"id" , "funds", "time" , "accountType","transactionType"})
 public class UserTransactionHistory {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_transaction_history_id")
     private Long id;
 
     private Long funds;
+
+    @CreatedDate
     private LocalDateTime time;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -34,5 +38,15 @@ public class UserTransactionHistory {
     private AccountType accountType;
     @Enumerated(EnumType.STRING)
     private TransactionType transactionType;
+
+    @Builder
+    public UserTransactionHistory(Long funds, User user, AccountType accountType, TransactionType transactionType){
+        this.funds = funds;
+        String customLocalDateTimeFormat = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.time = LocalDateTime.parse(customLocalDateTimeFormat, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        this.user = user;
+        this.accountType = accountType;
+        this.transactionType = transactionType;
+    }
 
 }
