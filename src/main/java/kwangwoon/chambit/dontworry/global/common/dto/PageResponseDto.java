21d59bdfb6c2cad2bbc5651aa60b15262e58c83e
simjaeyoun <spqjfgogo@gmail.com>
2024-09-30 20:43:10 +0900

    [Feat] Page인터페이스 프론트가 필요한 정보추려서 dto생성

---
+++ src/main/java/kwangwoon/chambit/dontworry/global/common/dto/PageResponseDto.java
@@ -0,0 +1,38 @@
+package kwangwoon.chambit.dontworry.global.common.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+@Data
+public class PageResponseDto<T> {
+    List<T> content;
+    PageInfo pageInfo;
+
+    public PageResponseDto(Page<T> page){
+        this.content = page.getContent();
+
+        this.pageInfo = new PageInfo(page.getPageable().getPageNumber()
+        ,page.getSize(), page.hasNext(), page.getTotalElements(), page.getTotalPages());
+    }
+
+    @Data
+    public static class PageInfo{
+        Integer currentPage;
+        Integer size;
+        Boolean hasNextPage;
+        Long totalContents;
+        Integer totalPages;
+
+        public PageInfo(int currentPage, int size, boolean hasNextPage, long totalContents, int totalPages){
+            this.currentPage = currentPage;
+            this.size = size;
+            this.hasNextPage = hasNextPage;
+            this.totalContents = totalContents;
+            this.totalPages = totalPages;
+        }
+    }
+}
