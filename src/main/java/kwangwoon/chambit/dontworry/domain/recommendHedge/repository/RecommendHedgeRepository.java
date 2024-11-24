be281cd66a275f2ad885d17f569c84ccb3a09807
simjaeyoun <spqjfgogo@gmail.com>
2024-11-24 19:16:54 +0900

    [Feat] 헷지상품 추천 테이블 생성 및 service와 repository기능 작성

---
+++ src/main/java/kwangwoon/chambit/dontworry/domain/recommendHedge/repository/RecommendHedgeRepository.java
@@ -0,0 +1,25 @@
+package kwangwoon.chambit.dontworry.domain.recommendHedge.repository;
+
+import kwangwoon.chambit.dontworry.domain.recommendHedge.domain.RecommendHedge;
+import kwangwoon.chambit.dontworry.domain.user.enums.HedgeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RecommendHedgeRepository extends JpaRepository<RecommendHedge, Long> {
+    @Query("select r from RecommendHedge r where r.stock.id=:stockId and r.hedgeType=:hedgeType")
+    Optional<RecommendHedge> findByStockIdAndHedgeType(@Param("stockId") Long stockId, @Param("hedgeType")HedgeType hedgeType);
+
+    @Query(value = "SELECT r.stock_id, r.hedge_type, r.risk_reduction_rate, d.derivative_name, d.derivative_code, s.stock_name, s.image_url " +
+            "FROM recommend_hedge r " +
+            "JOIN stock s ON r.stock_id = s.stock_id " +
+            "JOIN derivative d ON r.derivative_id = d.derivative_id " +
+            "WHERE JSON_CONTAINS(:pairs, JSON_OBJECT('stock_id', r.stock_id, 'hedge_type', r.hedge_type))",
+            nativeQuery = true)
+    List<Object[]> findByStockIdAndHedgeTypeIn(@Param("pairs") String jsonPairs);
+
+
+}
---
+++ src/main/java/kwangwoon/chambit/dontworry/domain/recommendHedge/service/RecommendHedgeService.java
@@ -0,0 +1,70 @@
+package kwangwoon.chambit.dontworry.domain.recommendHedge.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kwangwoon.chambit.dontworry.domain.portfolio.domain.Portfolio;
+import kwangwoon.chambit.dontworry.domain.portfolio.dto.response.dto.PortfolioRecommendDerivativeDto;
+import kwangwoon.chambit.dontworry.domain.recommendHedge.domain.RecommendHedge;
+import kwangwoon.chambit.dontworry.domain.recommendHedge.repository.RecommendHedgeRepository;
+import kwangwoon.chambit.dontworry.domain.user.enums.HedgeType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendHedgeService {
+    private final RecommendHedgeRepository recommendHedgeRepository;
+
+    public List<PortfolioRecommendDerivativeDto> getAllRecommendDerivative(List<Portfolio> portfolios) throws JsonProcessingException {
+        return getRecommendDerivative(portfolios, Integer.MAX_VALUE);
+    }
+    public List<PortfolioRecommendDerivativeDto> getTwoRecommendDerivative(List<Portfolio> portfolios) throws JsonProcessingException {
+        return getRecommendDerivative(portfolios, 2);
+    }
+
+    private List<PortfolioRecommendDerivativeDto> getRecommendDerivative(List<Portfolio> portfolios, int maxValue) throws JsonProcessingException {
+        Map<String, Long> quantities = new HashMap<>();
+        List<Map<String, Object>> pairs = getPairs(portfolios, quantities, maxValue);
+        return getPortfolioRecommendDerivativeDtos(pairs, quantities);
+    }
+
+    private List<Map<String, Object>> getPairs(List<Portfolio> portfolios, Map<String, Long> quantities, int limit) {
+        List<Map<String, Object>> pairs = portfolios.stream()
+                .sorted((p1,p2) -> Long.compare(p2.getStockQuantity(), p1.getStockQuantity()))
+                .limit(limit)
+                .map(portfolio -> {
+                    Object stockId = portfolio.getStock() != null ? portfolio.getStock().getId() : null;
+                    Object hedgeType = portfolio.getUser() != null ? portfolio.getUser().getHedgeType().name() : null;
+
+                    quantities.put(String.valueOf(stockId) + String.valueOf(hedgeType), portfolio.getStockQuantity());
+
+                    return Map.of("stock_id", stockId, "hedge_type", hedgeType);
+                })
+                .collect(Collectors.toList());
+        return pairs;
+    }
+
+    private List<PortfolioRecommendDerivativeDto> getPortfolioRecommendDerivativeDtos(List<Map<String, Object>> pairs, Map<String, Long> quantities) throws JsonProcessingException {
+        String jsonPairs = new ObjectMapper().writeValueAsString(pairs);
+        List<Object[]> recommendHedges = recommendHedgeRepository.findByStockIdAndHedgeTypeIn(jsonPairs);
+        List<PortfolioRecommendDerivativeDto> result = new ArrayList<>();
+        for(var hedge : recommendHedges){
+            String quantityKey = String.valueOf(hedge[0]) + String.valueOf(hedge[1]);
+            Long quantity = quantities.get(quantityKey);
+
+            PortfolioRecommendDerivativeDto dto = new PortfolioRecommendDerivativeDto(hedge, quantity);
+
+            result.add(dto);
+        }
+
+        return result;
+    }
+
+}
