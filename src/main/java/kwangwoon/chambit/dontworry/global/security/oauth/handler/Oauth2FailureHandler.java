241e9e91018058036ec038acee2f5f77e5a54a0a
simjaeyoun <spqjfgogo@gmail.com>
2024-12-19 21:09:19 +0900

    [Refactor] refer헤더에 따른 리다이렉션 주소 설정

--- src/main/java/kwangwoon/chambit/dontworry/global/security/oauth/handler/Oauth2FailureHandler.java
+++ src/main/java/kwangwoon/chambit/dontworry/global/security/oauth/handler/Oauth2FailureHandler.java
@@ -9,6 +9,8 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static kwangwoon.chambit.dontworry.global.config.DomainConfig.FrontServer;
 import static kwangwoon.chambit.dontworry.global.config.DomainConfig.LocalHttp;
@@ -17,11 +19,30 @@ import static kwangwoon.chambit.dontworry.global.config.DomainConfig.LocalHttp;
 public class Oauth2FailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        if(request.getServerName().equals("localhost")){
-            response.sendRedirect(LocalHttp.getAddress());
+
+        String referer = request.getHeader("Referer"); // Referer 헤더 값 가져오기
+        String refererHost = "";
+        if (referer != null) {
+            try {
+                // URL에서 호스트와 포트만 추출
+                URL refererUrl = new URL(referer);
+                refererHost = refererUrl.getProtocol() + "://" + refererUrl.getHost() + "/";
+                if (refererUrl.getPort() != -1) {
+                    refererHost += ":" + refererUrl.getPort(); // 포트가 존재하면 추가
+                }
+            } catch (MalformedURLException e) {
+                System.err.println("Invalid Referer URL: " + referer);
+            }
         }
-        else{
+
+        System.out.println("Processed Referer: " + refererHost);
+
+        if(refererHost.isEmpty() || refererHost.equals(LocalHttp.getAddress())){
+            response.sendRedirect(LocalHttp.getAddress());
+        } else if(refererHost.equals(FrontServer.getAddress())){
             response.sendRedirect(FrontServer.getAddress());
+        } else{
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Unknown state");
         }
     }
 }
